871a51cc055475d32889038990ee2f20faf39b06
Deacon Hughes <deaconhughes07@gmail.com>
2022-11-17 17:07:42 +0000

    Fixed nav properly

--- js/navController.js
+++ js/navController.js
@@ -2,25 +2,30 @@
   const burger = document.getElementById("burger");
   const dropDown = document.querySelectorAll(".Dropdown");
   const nav = document.querySelector("nav");
+  const navHeight = nav.offsetHeight;
+  let dropDownHeight = document.querySelector(".dropdownContent").offsetHeight;
+  console.info(dropDownHeight);
+  console.info(navHeight);
 
   for (let i = 0; i < dropDown.length; i++) {
     dropDown[i].addEventListener("click", (ev) => {
       ev.preventDefault();
       let currentDropDown = ev.target;
-      let query = "." + currentDropDown.classList[1].toLowerCase() + "DropdownContent";
+      let query =
+        "." + currentDropDown.classList[1].toLowerCase() + "DropdownContent";
       let dropdownContent = document.querySelector(query);
 
       if (dropdownContent.style.height == "0em") {
         let allOtherDropDown = document.querySelectorAll(".dropdownContent");
-        allOtherDropDown.forEach(element => {
-            element.style.height = "0em";
+        allOtherDropDown.forEach((element) => {
+          element.style.height = "0em";
         });
+
         dropdownContent.style.height = "4.75em";
-        nav.style.height = "19em";
-        
+        nav.style.height = navHeight - (dropDownHeight * (dropDown.length - 2)) + "px";
       } else {
         dropdownContent.style.height = "0em";
-        nav.style.height = "14em";
+        nav.style.height = navHeight - (dropDownHeight * (dropDown.length - 1)) + "px";
       }
     });
     dropDown[i].click();
@@ -28,12 +33,12 @@
 
   burger.addEventListener("click", () => {
     if (nav.style.height == "0em") {
-      nav.style.height = "14em";
+      nav.style.height =  navHeight - (dropDownHeight * (dropDown.length - 1)) + "px";
     } else {
       nav.style.height = "0em";
       let allDropDown = document.querySelectorAll(".dropdownContent");
-      allDropDown.forEach(element => {
-          element.style.height = "0em";
+      allDropDown.forEach((element) => {
+        element.style.height = "0em";
       });
     }
   });
