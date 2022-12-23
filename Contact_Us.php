bdc7977209e6743c26bee770ad4cbb8aa5b573d0
Deacon Hughes <deaconhughes07@gmail.com>
2022-12-23 22:55:26 +0000

    Fixed some link styling added start of contact us

--- Contact_Us.php
+++ Contact_Us.php
@@ -5,9 +5,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cantor College || Contact Us</title>
+    <link rel="stylesheet" href="css/mobile.css">
 </head>
 <body>
-    
+    <div class="container">
+        <header>
+            <?php include("includes/header.php"); ?>
+        </header>
+        <main id="NonHome">
+            <!-- Second Section On Page -->
+            <section></section>
+            <!-- First Section On Page -->
+            <section></section>
+        </main>
+        <footer>
+            <?php include("includes/footer.php"); ?>
+        </footer>
+    </div>
 </body>
 </html>
